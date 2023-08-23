a5ab7f5f28278c4d578b7dbd89d952704ef1fb17
yuvaraj0705 <113332600+yuvaraj0705@users.noreply.github.com>
2023-08-23 09:59:03 +0530

    Add files via upload

---
+++ logistic regression.py
@@ -0,0 +1,34 @@
+from sklearn.datasets import make_classification
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+
+x, y = make_classification(
+    n_samples=100,
+    n_features=1,
+    n_classes=2,
+    n_clusters_per_class=1,
+    flip_y=0.03,
+    n_informative=1,
+    n_redundant=0,
+    n_repeated=0
+)
+print(y)
+
+plt.scatter(x, y, c=y, cmap='rainbow')
+plt.title('Scatter Plot of Logistic Regression')
+plt.show()
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+
+x_train.shape
+
+log_reg = LogisticRegression()
+log_reg.fit(x_train, y_train)
+
+y_pred = log_reg.predict(x_test)
+
+confusion_matrix(y_test, y_pred)
